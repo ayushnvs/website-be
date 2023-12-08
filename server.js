4312bda2165bc4dba8ccc584826237a70ef48e29
ayushnvs <ayush.mishra.ald@gmail.com>
2023-12-08 23:36:54 +0530

    Update

--- server.js
+++ server.js
@@ -10,18 +10,22 @@ const user = require('./routes/user')
 require('dotenv').config()
 
 const app = express();
+
 const port = process.env.PORT || 8000
-
-app.use(logger)
-app.use(cors(corsOption))
-app.use(express.json())
-
 const uri = process.env.ATLAS_URI
 connectDB(uri)
 
+// Middlewares
+app.use(logger)
+app.use(cors(corsOption))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// APIs
 app.use('/account', auth)
 app.use('/user', user)
 
+// Middlewares
 app.use(errorHandler)
 
 app.listen(port, () => {
