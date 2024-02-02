fa5553717782e67f62eae7049e5b9c802d2e4cdb
ayushnvs <ayush.mishra.ald@gmail.com>
2024-02-02 23:05:18 +0530

    Registration Update

--- controller/registerController.js
+++ controller/registerController.js
@@ -1,4 +1,5 @@
 const User = require('../db/userModel')
+const Profile = require('../db/profileModel')
 const bcrypt = require('bcrypt')
 const { genAccessToken, genRefreshToken } = require('./tokenController')
 
@@ -10,16 +11,18 @@ const registerNewUser = async (req, res) => {
     if (!isUserAvailable) {
         bcrypt.hash(password, saltRounds).then((hash) => {
             const user = new User({ name, email, username, password: hash })
+            const profile = new Profile({ name, email, username })
             user.save()
                 .then(async () => {
                     const refreshToken = genRefreshToken(user.username)
-                    res.cookie('jwt', refreshToken, {httpOnly: true, sameSite: 'None', secure: true, maxAge: 2 * 60 * 60 * 1000})
+                    res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 2 * 60 * 60 * 1000 })
                     res.json({
                         message: 'Registration Successful',
                         token: genAccessToken(user.username)
                     })
                     user.refreshToken = refreshToken
                     await user.save()
+                    await profile.save()
                 })
                 .catch(err => res.status(400).json('Error: ' + err))
         })
